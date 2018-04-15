8dd13d737de916087421cba4fd8819e64fe75417
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-04-15 10:10:55 +0200

    dodanie MainControllera

---
+++ Zadanie 3/restService/src/main/java/kolos/controllers/MainControllerKolos.java
@@ -0,0 +1,17 @@
+package kolos.controllers;
+
+import kolos.controllers.responseEntry.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+@Controller @RequiredArgsConstructor public class MainControllerKolos {
+
+
+}
