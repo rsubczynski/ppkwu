61d4f39809e6770a48e216803ca29e64f06140cc
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-04-15 10:35:48 +0200

    edycja metod w main controlerze

--- Zadanie 3/restService/src/main/java/kolos/controllers/MainControllerKolos.java
+++ Zadanie 3/restService/src/main/java/kolos/controllers/MainControllerKolos.java
@@ -1,40 +1,47 @@
 package kolos.controllers;
 
-import kolos.controllers.responseEntry.*;
+import kolos.controllers.responseEntry.BaseResponse;
+import kolos.controllers.responseEntry.Circle;
+import kolos.controllers.responseEntry.Square;
+import kolos.controllers.responseEntry.Triangle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 @Controller @RequiredArgsConstructor public class MainControllerKolos {
 
-    @RequestMapping("/v2/drawTheMethod") public @ResponseBody BaseResponse drawTheMethod(
+    @RequestMapping("/v1/drawTheMethod") public @ResponseBody BaseResponse drawTheMethod(
             @RequestParam("mail")
                     String mail) {
-        int random = (int) (Math.random() * 2 );
+        int random = (int) (Math.random() * 2);
         switch (random) {
             case 0:
                 return new EmailResponse(isValidEmail(mail));
             case 1:
-                List<String> apiList = new ArrayList<>();
-                apiList.add("/v2/checkMail");
-                apiList.add("/v2/drawFigure");
-                return ApiListResponse.builder().apiListResponse(apiList).randomFigure("Wylosowano figurę").build();
+                return drawFigure();
             default:
                 break;
         }
         return null;
     }
 
-    @RequestMapping("/v2/checkMail") public @ResponseBody EmailResponse sendMail(
-            @RequestParam("mail")
-                    String mail) {
-        return new EmailResponse(isValidEmail(mail));
+    private @ResponseBody BaseResponse drawFigure() {
+        int random = (int) (Math.random() * 3);
+        switch (random) {
+            case 0:
+                return new Circle(3.6f, "Koło");
+            case 1:
+                return new Square(3.6f, "Kwadrat");
+            case 2:
+                return new Triangle(3.6f, 1, "Trójkąt");
+            default:
+                break;
+        }
+        return null;
     }
 
     private static boolean isValidEmail(String email) {
